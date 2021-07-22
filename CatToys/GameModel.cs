e199e5d8ef15fb9b4969e5d0a633a3af307cb4bb
jstocks01 <dicipleofrollins@gmail.com>
2021-07-21 17:52:29 -0700

    updated details page

--- CatToys/GameModel.cs
+++ CatToys/GameModel.cs
@@ -6,23 +6,25 @@ namespace CatToys
     public class GameModel
     {
         public int Id { get; set; }
-        public string Name { get; set;}
-        public string description { get; set;}
+        public string Name { get; set; }
+        public string description { get; set; }
         public string launchDate { get; set; }
         public string minAge { get; set; }
         public string numberOfPeople { get; set; }
-        
+
         public string timeRange { get; set; }
 
         public GameType GameType { get; set; }
 
-        public  string image { get; set; }
+        public string image { get; set; }
 
         public string imageAlt { get; set; }
 
         public string fulldescription { get; set; }
 
         public string fullimage { get; set; }
+        public string BGGlink { get; set; }
+        public string sellSheet { get; set; }
 
     }
 }
